d12da1bf8aa55e52b09683660189623d26e1988e
emilioNova <37042917+emilioNova@users.noreply.github.com>
2022-06-22 20:15:27 +0200

    Accesibilidad

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,17 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  framework: "@storybook/angular",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
-  features: {
-    interactionsDebugger: true,
-  },
-};
+ 
+   stories: ['../src/app/components/**/*.stories.ts'],
+    addons: [
+      '@storybook/addon-links',
+      '@storybook/addon-essentials',
+      '@storybook/addon-interactions',
+      '@storybook/addon-a11y',
+    ],
+    framework: '@storybook/angular',
+    core: {
+      builder: '@storybook/builder-webpack5',
+    },
+    features: {
+      interactionsDebugger: true,
+    },
+  };
